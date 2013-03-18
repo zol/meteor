a6a8c694d1aeda038367e6bc71209261ad589397
Geoff Schmidt <geoff@geoffschmidt.com>
2013-03-17 19:40:35 -0700

    Linker prints pretty banners/line numbers

--- engine/linker.js
+++ engine/linker.js
@@ -1,32 +1,58 @@
 var fs = require('fs');
 var _ = require('underscore');
 
-// options include:
-//
-// inputFiles: an array of objects representing input files.
-//  - source: the source code
-//  - servePath: the path where it would prefer to be served if possible
-//
-// useGlobalNamespace: make the top level namespace be the same as the
-// global namespace, so that symbols are accessible from the
-// console. typically used when linking apps (as opposed to packages.)
-//
-// combinedServePath: if we end up combining all of the files into
-// one, use this as the servePath.
-//
-// Output is an array of output files in the same format as
-// 'inputFiles'.
-
-var wrapFile = function (source) {
+// Options:
+// - preserveLineNumbers: if true, decorate minimally so that line
+//   numbers don't change between input and output
+// - path: a (cosmetic) path to print in the header. the first
+//   character will be stripped, on the assumption that it's '/'
+// - sourceWidth: width in columns to use for the source code
+var wrapFile = function (source, options) {
   // The newline after the source closes a '//' comment.
   //
   // The ".call(this)" allows you to do a top-level "this.foo = " to
   // define global variables; this is the only way to do it in
   // CoffeeScript.
-  //
-  // XXX add a line number comment to each line, in case we don't have
-  // source maps, but only if we will combine the files later?
-  return "(function(){" + source + "\n}).call(this);\n";
+
+  if (options.preserveLineNumbers) {
+    return "(function(){" + source + "\n}).call(this);\n";
+  } else {
+    var ret = "";
+
+    // Prologue
+    ret += "(function () {\n\n";
+
+    // Banner
+    var width = options.sourceWidth || 70;
+    var bannerWidth = width + 3;
+    var divider = new Array(bannerWidth + 1).join('/') + "\n";
+    var spacer = "// " + new Array(bannerWidth - 6 + 1).join(' ') + " //\n";
+    var padding = new Array(bannerWidth + 1).join(' ');
+    var blankLine = new Array(width + 1).join(' ') + " //\n";
+    ret += divider + spacer;
+    ret += "// " + (options.path.slice(1) + padding).slice(0, bannerWidth - 6) +
+      " //\n";
+    ret += spacer + divider + blankLine;
+
+    // Code, with line numbers
+    // You might prefer your line numbers at the beginning of the
+    // line, with /* .. */. Well, that requires parsing the source for
+    // comments, because you have to do something different if you're
+    // already inside a comment.
+    var lines = source.split('\n');
+    var num = 1;
+    _.each(lines, function (line) {
+      ret += (line + padding).slice(0, width) + " // " + num + "\n";
+      num++;
+    });
+
+    // Footer
+    ret += divider;
+
+    // Epilogue
+    ret += "\n}).call(this);\n\n\n\n\n\n"
+    return ret;
+  }
 };
 
 // file should have a 'source' attribute. Compute the global
@@ -43,15 +69,47 @@ var computeGlobalReferences = function (file) {
   file.globalReferences = {};
 };
 
+var maxLineLengthInFiles = function (files) {
+  var maxInFile = [];
+  _.each(files, function (file) {
+    var lines = file.source.split('\n');
+    maxInFile.push(_.max(_.pluck(lines, "length")));
+  });
+
+  return _.max(maxInFile);
+};
+
+// options include:
+//
+// inputFiles: an array of objects representing input files.
+//  - source: the source code
+//  - servePath: the path where it would prefer to be served if possible
+//
+// useGlobalNamespace: make the top level namespace be the same as the
+// global namespace, so that symbols are accessible from the
+// console. typically used when linking apps (as opposed to packages.)
+//
+// combinedServePath: if we end up combining all of the files into
+// one, use this as the servePath.
+//
+// Output is an array of output files in the same format as
+// 'inputFiles'.
 var link = function (options) {
   var files = _.map(options.inputFiles, _.clone);
 
   if (! files.length)
     return [];
 
+  // Find the maximum line length
+  var sourceWidth = _.max([70, maxLineLengthInFiles(files)]);
+
   // Wrap each file in its own namespace
   _.each(files, function (file) {
-    file.source = wrapFile(file.source);
+    file.source = wrapFile(file.source, {
+      path: file.servePath,
+      preserveLineNumbers: options.useGlobalNamespace,
+      sourceWidth: sourceWidth
+    });
   });
 
   // If not using the global namespace, create a second namespace that
@@ -74,8 +132,6 @@ var link = function (options) {
 
     // Emit each file
     _.each(files, function (file) {
-      // XXX make banner prettier
-      combined += "/* " + file.servePath + " */\n";
       combined += file.source;
       combined += "\n";
     });
